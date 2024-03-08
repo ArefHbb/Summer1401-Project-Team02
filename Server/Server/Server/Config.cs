a6fc7090a8f77367253de5b256fe487b0f74c8e4
arefhbb <haref3460@gmail.com>
2024-03-08 21:16:14 +0330

    change config.cs

--- Server/Server/Server/Config.cs
+++ Server/Server/Server/Config.cs
@@ -3,10 +3,10 @@ namespace Server;
 public static class Config
 {
  
-    public const string Server = "185.220.224.152";
+    public const string Server = "localhost";
     public const string DataBase = "etl";
     public const string Id = "sa";
-    public const string Password = "SuperSecureP4ssw0rd";
+    public const string Password = "1234";
     public const string dataInventoryTableName = "AllDatasetsInventory";
     
     public const int PreviewCapacity = 20;  
